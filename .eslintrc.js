0bd433f1e27daa41c20dc2206f807dbe303e8bcb
Lukas Bach <lbach@outlook.de>
2022-03-31 00:49:13 +0200

    linter setup fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -20,13 +21,11 @@ module.exports = {
   ignorePatterns: ["*.js", "*.d.ts"],
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    "prettier/prettier": "error",
-    indent: ["error", 2],
     "linebreak-style": "off",
     quotes: ["error", "double"],
-    semi: ["error", "always"],
     "react/prop-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off" // TODO
+    "@typescript-eslint/no-unused-vars": "off", // TODO
+    "@typescript-eslint/ban-types": "off",
   },
 };
