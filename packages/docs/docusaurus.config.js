979ec990c1794a91a5334963b803bf2ffc84aed8
Lukas Bach <lbach@outlook.de>
2022-04-03 01:12:20 +0200

    temp update docs url

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Synergies',
   tagline: 'Synergyze your state!',
-  url: 'https://synergies.lukasbach.com',
-  baseUrl: '/',
+  url: 'https://lukasbach.github.io/', // TODO
+  baseUrl: '/synergies/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
